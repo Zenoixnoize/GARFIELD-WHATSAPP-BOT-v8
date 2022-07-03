891b6a464f8d4f6eb13f28fe0231e5b51a68cfad
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-07-03 12:58:24 +0530

    ＥＤＩＴ  ＴＨＩＳ  ＦＩＬＥ  🦋

--- Config.js
+++ Config.js
@@ -10,40 +10,39 @@ global.APIs = {
 global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01',  // Do not change
 }
-global.owner = ["+94711502119"] //👈  Enter Your number)
-global.premium = ["+94711502119"] // 👈  Enter Your number)
-global.emoji01 = ["🐼"] // 👈 Enter your Emoji 
-global.emoji02 = ["🦋"] //👈 Enter your Emoji
+global.owner = ['+94711502119'] //👈  Enter Your number)
+global.premium = ['+94711502119'] // 👈  Enter Your number)
+global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
+global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.ownernomer = [+94711502119] //👈 Enter Your number
-global.ownername = ["ZENOI"] //👈 Enter Your name
-global.botname = ["ＧＡＲ ＦＩＥＬＤ ＢＯＴ ｖ8.2"] //👈 Enter Your Bot name
-global.footer = ["Coded by Tharindu Liyanage"] //👈 Enter Your Name
-global.ig = ["https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8"]
-global.region = ["India"] // 👈 Enter Your country
-global.sc = ["https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8"]
-global.myweb = ["https://www.facebook.com/garfieldbots/"] // 👈 Enter your Social media link to follow now button
-global.packname = ["GARFIELD-BOTv8"]  // 👈 You Can change this your choice 
-global.author = ["NOIZE DEVELOPMENT"] // 👈 You Can change this your choice 
+global.ownername = ['ZENOI'] //👈 Enter Your name
+global.botname = ['ＧＡＲ ＦＩＥＬＤ ＢＯＴ ｖ8.2'] //👈 Enter Your Bot name
+global.footer = ['Coded by Tharindu Liyanage'] //👈 Enter Your Name
+global.ig = ['https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8']
+global.region = ['India'] // 👈 Enter Your country
+global.sc = ['https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8']
+global.myweb = ['https://www.facebook.com/garfieldbots/'] // 👈 Enter your Social media link to follow now button
+global.packname = ['GARFIELD-BOTv8']  // 👈 You Can change this your choice 
+global.author = ['NOIZE DEVELOPMENT'] // 👈 You Can change this your choice 
 global.sessionName = 'session'  // 👈 You Can change this your choice 
 global.prefa = ['','!','.']  // 👈 You Can change this your choice 
 global.sp = ['ZENOI']  // 👈 You Can change this your choice 
-global.plwt = ["```Please Wait```"]
 
 global.mess = {
-    success: ["Done 🐼"], //👈 Use these in your language of choice
-    admin: ["This Feature Is Only For Admin!"], // 👈 Use these in your language of choice
-    botAdmin: ["Bot Must Be Admin First!"], // 👈 Use these in your language of choice
-    owner: ["This Feature Is Only For Owner!"], // 👈 Use these in your language of choice
-    group: ["Feature Used Only For Groups!"], //👈 Use these in your language of choice
-    private: ["Features Used Only For Private Chat!"], // 👈 Use these in your language of choice
-    bot: ["This Feature Is Only For Bot"], //👈 Use these in your language of choice
-    wait: '```Please Wait 🐼```', // 👈 Use these in your language of choice
-    error: ["Error! Maybe Api Key Is Expired 🐼!"], // 👈 Use these in your language of choice
-    endLimit: ["Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours"],
+    success: ['Done 🐼'], //👈 Use these in your language of choice
+    admin: ['This Feature Is Only For Admin!'], // 👈 Use these in your language of choice
+    botAdmin: ['Bot Must Be Admin First!'], // 👈 Use these in your language of choice
+    owner: ['This Feature Is Only For Owner!'], // 👈 Use these in your language of choice
+    group: ['Feature Used Only For Groups!'], //👈 Use these in your language of choice
+    private: ['Features Used Only For Private Chat!'], // 👈 Use these in your language of choice
+    bot: ['This Feature Is Only For Bot'], //👈 Use these in your language of choice
+    wait: ['```Please Wait 🐼```'], // 👈 Use these in your language of choice
+    error: ['Error! Maybe Api Key Is Expired 🐼!'], // 👈 Use these in your language of choice
+    endLimit: ['Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours'],
 }
     global.limitawal = { 
-    premium: "Infinity",
-    free: "infinity",
+    premium: 'Infinity',
+    free: 'infinity',
     monayawal: 1000
 }
    global.rpg = {
@@ -64,18 +63,18 @@ global.flasmurf = 'https://i.ibb.co/Jz0mMnC/Pics-Art-22-06-03-01-01-46-876.png' 
 
 // Do not change 👇 
 
-global.limitAwal =  "99999999999999999",  //Do not Change 🚫
-global.lolkey =  "835641f3d2d1b6971e1fd132", //Do not Change 🚫
-global.xteam =  "apivproject", //Do not Change 🚫
-global.xcodeapi =  "5S3epf7hC2", //Do not Change 🚫
-global.dapaapi =  "piceg", //Do not Change 🚫
-global.sankey =  "sanuwa", //Do not Change 🚫
-global.xteamkey =  "nandowangy", //Do not Change 🚫
-global.carkey =  "cakrayp24Q6", //Do not Change 🚫
-global.violkey =  "beta", //Do not Change 🚫
-global.HunterApi =  "FuckBitch", //Do not Change 🚫
-global.neokey =  "V8qnSaTq", //Do not Change 🚫
-global.riy =  "RiyGanz" //Do not Change 🚫
+global.limitAwal =  '99999999999999999',  //Do not Change 🚫
+global.lolkey =  '835641f3d2d1b6971e1fd132', //Do not Change 🚫
+global.xteam =  'apivproject', //Do not Change 🚫
+global.xcodeapi =  '5S3epf7hC2', //Do not Change 🚫
+global.dapaapi =  'piceg', //Do not Change 🚫
+global.sankey =  'sanuwa', //Do not Change 🚫
+global.xteamkey =  'nandowangy', //Do not Change 🚫
+global.carkey =  'cakrayp24Q6', //Do not Change 🚫
+global.violkey =  'beta', //Do not Change 🚫
+global.HunterApi =  'FuckBitch', //Do not Change 🚫
+global.neokey =  'V8qnSaTq', //Do not Change 🚫
+global.riy =  'RiyGanz' //Do not Change 🚫
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
